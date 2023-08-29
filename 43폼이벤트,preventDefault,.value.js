0d666c92377addac1c4a423713274e81aaf4f413
jihyun Choi <wlgusl1@naver.com>
2023-08-29 09:25:30 +0900

    ~~!

--- 43폼이벤트,preventDefault,.value.js
+++ 43폼이벤트,preventDefault,.value.js
@@ -88,3 +88,127 @@ input2.addEventListener('input', function () {  //입력창에 입력하면
 // })
 //이때 틀린부분이 두군데있었는데 둘다 ; 세미콜론때문.
 //if(input.value==='';) 세미콜론쓰면안됨   맨마지막에 ${input.value};`} 이세미콜론도안됨
+
+
+// 버블링     물속에서 올라오는 공기방울처럼 맨아래 요소부터 높은레벨까지 올라가는 것을 버블이라한다.
+// 버튼 클릭하면 버튼작동-> p작동 ->섹션작동    p클릭하면 p작동-> 섹션작동   
+//<section onclick="alert('section clicked')">
+//  <p onclick="alert('paragraph clicked')">I am a paragraph
+//        <button onclick="alert('button clicked')">bubble</button>
+//  </p>
+//</section>
+
+const bubblediv = document.querySelector('#bubblediv');
+const bubblebtn = document.querySelector('#bubblebtn');
+bubblebtn.addEventListener('click', function (e) {    //버튼 클릭하면 함수실행
+    bubblediv.style.backgroundColor = makeRandColor()  //디브 배경색 랜덤컬러로.
+    e.stopPropagation();                   //이벤트가 버블링하지 않도록 막아줌
+})  //원래는 div이벤트까지 실행됐어야하는데 버블링을 막아서 배경색만 바꾸고 div의 이벤트는 실행안됨. 
+const makeRandColor = () => {                     //랜덤컬러 만듦
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+    //여기서 헤맸던 포인트. 버튼선택할때 querySelectorAll로 선택을했는데 랜덤컬러 작동이 안됐음
+    //querySelector로 바꾸니 작동됨. id는 하나니까 querySelector써도 됨.
+}
+
+bubblediv.addEventListener('click', function () {
+    bubblediv.classList.toggle('hide');     //디브에 hide클래스를 적용시키는데 토글시킨다
+})  //디브를 클릭하면 디브가 사라지게함.  토글이라 클릭하면 사라지고 클릭하면 다시생기는줄 알았는데
+//사라지면 다시안생긴다 왜그럴까?
+
+//stopPropagation()  이벤트가 버블링하지 않도록 막아줌
+
+
+
+//폼이벤트 트윗만들기
+const tweetForm = document.querySelector('#tweetform');
+const tweetsContainer = document.querySelector('#tweets');
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    //const usernameInput=document.querySelectorAll('input')[0]  이거보다 아래줄처럼쓰는게 좋다
+    const usernameInput = tweetForm.elements.username;   //username은 name에서 가져온값
+    const tweetInput = tweetForm.elements.tweet;      // input type="text" name="username"
+    addTweet(usernameInput.value, tweetInput.value) //input창에 입력한 입력값들
+    // added함수가 실행됨  /여기서 두 입력값은 아래 따로빼놓은 함수의 매개변수로 들어가서 실행됨 
+    usernameInput.value = '';
+    tweetInput.value = '';
+});
+//tweetInput.value = ''; 써서 입력창을 빈칸을 만들고싶었는데 
+//아래 added함수 맨아래에 쓰니까 작동이안돼서 위치를 이자리로 바꿨다.
+//원래는 added함수가 없이 함수가 하나였다 (아래 참조) 입력창 빈칸을 만들기위해
+//added함수를 따로빼서 만들고 tweetInput.value = '';위치를 바꿨더니 잘작동한다
+
+const addTweet = (username, tweet) => { //매개변수에 (usernameInput.value,tweetInput.value)
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)  //매개변수(usernameInput.value)
+    newTweet.append(bTag);
+    newTweet.append(`-${tweet}`);  //매개변수(tweetInput.value)
+    tweetsContainer.append(newTweet);
+}
+// name값 사용하기    const usernameInput = tweetForm.elements.username;
+// input type="text" name="username" 여기 이 name특성을 가져다 사용하는것.
+// console.dir(tweetForm)을 보면 elements(요소)라는 특성이 나온다
+// 코드를 적은 순서대로 요소들이 나오는데 (인덱스도포함) name에 적은 글자도 나온다.
+//0:input  1:input  2:button length:3  tweet:input username:input
+//그래서 name을 이용해 tweetform.elements.tweet.value 입력값을 찾을수있다.class를 tweet으로 정하고
+//입력값을 찾아봤는데 안됐다. 
+
+
+//이벤트위임  만들어놨던 트윗 수정하기
+const tweetForm1 = document.querySelector('#eventform');
+const tweetsContainer1 = document.querySelector('#evts');
+tweetForm1.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // const usernameInput = document.querySelectorAll('input')[0];
+    // const tweetInput = document.querySelectorAll('input')[1];
+    const usernameInput = tweetForm1.elements.user;
+    const tweetInput = tweetForm1.elements.tweett;
+    addTweet1(usernameInput.value, tweetInput.value)
+    usernameInput.value = '';
+    tweetInput.value = '';
+});
+
+const addTweet1 = (username, tweet) => {
+    const newTweet1 = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)
+    newTweet1.append(bTag);
+    newTweet1.append(`- ${tweet}`)
+    tweetsContainer1.append(newTweet1);
+}
+
+newTweet1.addEventListener('click', function () {
+    newTweet1.classList.toggle('hide')
+})
+
+
+
+
+
+//이벤트위임  만들어놨던 트윗 수정하기  원본
+// const tweetForm1 = document.querySelector('#eventform');
+// const tweetsContainer1 = document.querySelector('#evts');
+// tweetForm1.addEventListener('submit', function (e) {
+//     e.preventDefault();
+
+//     // const usernameInput = document.querySelectorAll('input')[0];
+//     // const tweetInput = document.querySelectorAll('input')[1];
+//     const usernameInput = tweetForm1.elements.user;
+//     const tweetInput = tweetForm1.elements.tweett;
+//     addTweet1(usernameInput.value, tweetInput.value)
+//     usernameInput.value = '';
+//     tweetInput.value = '';
+// });
+
+// const addTweet1 = (username, tweet) => {
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+//     bTag.append(username)
+//     newTweet.append(bTag);
+//     newTweet.append(`- ${tweet}`)
+//     tweetsContainer1.append(newTweet);
+// }
--- PRACTICE.js
+++ PRACTICE.js
@@ -8,27 +8,84 @@
 // greet1('"Hello','Barry')
 //이렇게 "Hello,Barry!" 나왔음 
 
-const day = 2;
-switch (day) {
-    case 1:
-        console.log("monday");
 
-    case 2:
-        console.log("tuesday");
 
-    case 3:
-        console.log("wednesday");
 
-    case 4:
-        console.log("thursday");
+const tweetForm = document.querySelector('#tweetform');
+const tweetsContainer = document.querySelector('#tweets');
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    //const usernameInput=document.querySelectorAll('input')[0]  이거보다 아래줄처럼쓰는게 좋다
+    const usernameInput = tweetForm.elements.username;   //username은 name에서 가져온값
+    const tweetInput = tweetForm.elements.tweet;      // input type="text" name="username"
+    added(usernameInput.value,tweetInput.value)
+    // added함수가 실행됨  (usernameInput.value,tweetInput.value) 
+   //여기서 두 입력값은 아래 따로빼놓은 함수의 매개변수로 들어가서 실행됨 
+    usernameInput.value = '';
+    tweetInput.value = '';
+})
+//tweetInput.value = ''; 써서 입력창을 빈칸을 만들고싶었는데 
+//아래 added함수 맨아래에 쓰니까 작동이안돼서 위치를 이자리로 바꿨다.
+//원래는 added함수가 없이 함수가 하나였다 (아래 참조) 입력창 빈칸을 만들기위해
+//added함수를 따로빼서 만들고 tweetInput.value = '';위치를 바꿨더니 잘작동한다
 
-    case 5:
-        console.log("friday");
 
-    case 6:
-    case 7:
-        console.log("Weekend!!")
 
-    default:
-        console.log("I don't know that.")
+    const added=(username,tweet)=>{  //매개변수에 (usernameInput.value,tweetInput.value)
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username)  //매개변수(usernameInput.value)
+    newTweet.append(bTag);  
+    newTweet.append(`-${tweet}`)  //매개변수(tweetInput.value)
+    tweetsContainer.append(newTweet);
+    
 }
+// console.dir(tweetForm)을 보면 elements(요소)라는 특성이 나온다
+// 코드를 적은 순서대로 요소들이 나오는데 (인덱스도포함) name에 적은 글자도 나온다.
+//0:input  1:input  2:button length:3  tweet:input username:input
+//그래서 name을 이용해 tweetform.elements.tweet.value 입력값을 찾을수있다.class를 tweet으로 정하고
+//입력값을 찾아봤는데 안됐다. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const input = document.querySelector('#catName')
+// const ulcat = document.querySelector('#cats')
+// form.addEventListener('submit', function (e) {
+//     e.preventDefault();      //폼제출하는 기본동작을 막음
+//     const catInput = input.value;  //입력값을 추출  
+//     const newLi = document.createElement('li');  //빈 li요소를 만들고
+//     newLi.innerText = catInput;  
+//     ulcat.append(newLi);   
+//     input.value = '';  //입력창을 빈칸으로 만듦
+//   
+// });  //얘는 입력창 빈칸이 되고
+
+
+// const tweetForm = document.querySelector('#tweetform');
+// const tweetsContainer = document.querySelector('#tweets');
+// tweetForm.addEventListener('submit', function (e) {
+//     e.preventDefault(); 
+//     const username = tweetForm.elements.username.value;   
+//     const tweet = tweetForm.elements.tweet.value;      
+    
+//     const newTweet = document.createElement('li');
+//     const bTag = document.createElement('b');
+//     bTag.append(username)
+//     newTweet.append(bTag);
+//     newTweet.append(`-${tweet}`)
+//     tweetsContainer.append(newTweet);
+//     username = '';
+//     tweet = '';
+// });  //얘는 입력창빈칸이 안됨 이유가 뭘까
