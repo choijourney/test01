a3bb02b5cc23689579a14e0953d8273c534b8eb5
jihyun Choi <wlgusl1@naver.com>
2023-08-31 11:31:11 +0900

    call stack

--- 44핑퐁점수관리자_코드리팩토링.js
+++ 44핑퐁점수관리자_코드리팩토링.js
@@ -17,11 +17,12 @@ let isGameOver = false;
 
 function updateScores(player, opponent) {  //괄호는 매개변수  밑에서 매개변수에 p1,p2를 적용시킬거다
     if (!isGameOver) {                  //isGameover의! 반대니까 game중이면(true)
-        player.score += 1;                    //total1값에 1을 더해라
-        if (player.score === winningScore) {  //total1이 winningScore랑 같으면
+        player.score += 1;             //player값에 1을 더해라
+
+        if (player.score === winningScore) {  //player이 winningScore랑 같으면
             isGameOver = true;          //let변수isGameOver를 true로 바꾼다
             player.display.classList.add('has-text-success'); //플레이어 디스플레이에 글자색바꾸는 클래스
-            opponent.display.classList.add('has-text-danger');  //추가
+            opponent.display.classList.add('has-text-danger');
             player.button.disabled = true; //플레이어 버튼 비활성화
             opponent.button.disabled = true;
         }
@@ -40,7 +41,7 @@ p2.button.addEventListener('click', function () {
 
 winningScoreSelect.addEventListener('change', function () { //change이벤트 변경이있을때 함수실행
     winningScore = parseInt(this.value); //변수winningScore를,셀렉트에입력한값을 숫자로바꾼값으로 업데이트
-    reset(); //셀렉트 숫자를 바꾸면 0 to 0 으로 리셋되게함
+    // reset(); //셀렉트 숫자를 바꾸면 0 to 0 으로 리셋되게함
 })
 
 btn3.addEventListener('click', reset)
@@ -56,3 +57,10 @@ function reset() {
 }
 //플레이어들이 많을수록 p.1.score; p.5.score; 이런식으로 계속나열해야하면 귀찮으니까
 //for of 로 반복시켜 코드를 줄인다 배열에 추가만 하면된다 [p1,p2,p3..]
+
+
+
+// 5점이 종료점수일때 4:4 듀스면  종료점수를 6으로 바꾸고싶음  (탁구는 2점차이가 나야 이기는게임이라고 들었다)
+// if (parseInt(winningScoreSelect.value - 1) === player.score === opponent.score) {
+//     parseInt(winningScoreSelect.value = +1)
+// }                 이코드가 실행이안된다 
